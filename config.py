b572615e79915751bb0cd2286d3c4fd622e0d071
pomice <134554554+pomicee@users.noreply.github.com>
2025-04-12 20:55:37 -0700

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,18 @@ import discord
 
 EMBED_COLOR = discord.Color.from_str("#1e1f22")
 ERROR_COLOR = discord.Color.from_str("#ff0000") 
-SUCCESS_COLOR = discord.Color.from_str("#00ff00") 
+SUCCESS_COLOR = discord.Color.from_str("#00ff00")
+
+LAVALINK_HOST = ""
+LAVALINK_PORT = 2333
+LAVALINK_PASSWORD = ""  
+LAVALINK_REST_URI = f"http://{LAVALINK_HOST}:{LAVALINK_PORT}"
+LAVALINK_WS_URI = f"ws://{LAVALINK_HOST}:{LAVALINK_PORT}"
+
+STATUS_MESSAGES = [
+    "!play | Music",
+    "with your heart ❤️",
+    "your favorite songs 🎵",
+    "the best music 🎶",
+    "in your server 🎸"
+]
